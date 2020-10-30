42e820a2d5b032453fbe691345665c47a6be599e
sonphan92 <phanxuanson92@gmail.com>
2020-10-30 07:59:31 +0700

    sua chuc nang search

--- d04-demo-event-home/index.js
+++ d04-demo-event-home/index.js
@@ -1,5 +1,9 @@
-window.onload = function() {};
+window.onload = function() {
+	
+}
 
+
+let listArrayName = [];
 function testEventFunc(e) {
 	e.preventDefault();
 	console.log('Vi Du Ham Nay de submit');
@@ -13,16 +17,65 @@ function isInputEmpty(inputName, inputValue) {
 	return false;
 }
 
+function onEdit(td) {
+	selectedRow = td.parentElement.parentElement;
+	currentEditEl = selectedRow;
+	document.getElementById('fullname').value = selectedRow.cells[0].innerHTML;
+	document.getElementById('phone-number').value = selectedRow.cells[1].innerHTML;
+	document.getElementById('address').value = selectedRow.cells[2].innerHTML;
+
+	// Lay cai add button va hide di
+	var btnAdd = document.getElementById('btnAdd');
+	btnAdd.setAttribute('style', 'display: none');
+
+	// Tao cai nut confirm roi add vao truoc nut btnCancel
+
+	var confirmBtn = document.createElement('input');
+	confirmBtn.className = 'btn btn-success btn-block';
+
+	// confirmBtn.setAttribute('onclick', 'onConfirmEdit(this)');
+	confirmBtn.addEventListener('click', onConfirmEdit);
+	confirmBtn.setAttribute('type', 'button');
+	confirmBtn.value = 'Confirm';
+
+	btnAdd.parentNode.insertBefore(confirmBtn, document.getElementById('btnCancel'));
+}
+
+var currentEditEl;
+
+function onConfirmEdit(event) {
+	var confirmBtn = event.target;
+	// confirmBtn.previousElementSibling.setAttribute('style', 'display: block');
+	confirmBtn.remove();
+	document.getElementById('btnAdd').setAttribute('style', 'display: block');
+	console.log(currentEditEl);
+
+	// lay gia tri thay doi cua form input -> ghi vao currentEditEl
+	currentEditEl.cells[0].innerHTML = document.getElementById('fullname').value;
+	currentEditEl.cells[1].innerHTML = document.getElementById('phone-number').value;
+	currentEditEl.cells[2].innerHTML = document.getElementById('address').value;
+
+	// location.reload();
+}
+
+function deleteRow(e) {
+	console.log(e);
+	var eventEl = e.target;
+	var parent2 = eventEl.parentNode.parentNode;
+	parent2.remove();
+}
+
 function AddData(e) {
+	let listName = [];
 	let name = document.getElementById('fullname').value;
 	let phone = document.getElementById('phone-number').value;
 	let address = document.getElementById('address').value;
 
-	 if (name.length < 2 || phone.length < 2 || address.length < 2) {
-	 alert('nhap ten day du');
-	   return false;
-	 }
-	
+	// if (name.length < 2 || phone.length < 2 || address.length < 2) {
+	// 	alert('nhap ten day du');
+	// 	return false;
+	// }
+
 	var inputArr = document.getElementsByTagName('input');
 
 	// Kiem tra du lieu co hay khong
@@ -31,7 +84,13 @@ function AddData(e) {
 	className[0].className = 'fullname-error error';
 	className[0].className = 'fullname-error error';
 	className[0].className = 'fullname-error error';
+	listName.push({
+		name,
+		phone,
+		address
+	})
 
+	render(listName);
 	// Duyet qua mang chua input element
 	// Kiem tra input element co thoa man dieu kien validate
 	// Co thi continue
@@ -51,14 +110,7 @@ function AddData(e) {
 	let tbodyToWrite = document.getElementById('tbodyToWrite');
 
 	let row = document.createElement('tr');
-	row.innerHTML = `<td>${name}</td>
-           <td>${phone}</td>
-           <td>${address}</td>
-           <td>
-             <button class="btn btn-danger" onclick="deleteRow(event)" style="display:none">x</button>
-             <button class="btn btn-warning editRowBtn" onclick="onEdit(this)">Edit</button>
-             <button class="btn btn-danger deleteRowBtn">x</button>
-           </td>`;
+	row.innerHTML = listArrayName.join("");
 
 	// Cho ni la add dong moi vo
 	tbodyToWrite.appendChild(row);
@@ -69,54 +121,24 @@ function AddData(e) {
 		row.onclick = deleteRow;
 	}
 }
-
-function onEdit(td) {
-	selectedRow = td.parentElement.parentElement;
-	currentEditEl = selectedRow;
-	document.getElementById('fullname').value = selectedRow.cells[0].innerHTML;
-	document.getElementById('phone-number').value = selectedRow.cells[1].innerHTML;
-	document.getElementById('address').value = selectedRow.cells[2].innerHTML;
-
-	// Lay cai add button va hide di
-	var btnAdd = document.getElementById('btnAdd');
-	btnAdd.setAttribute('style', 'display: none');
-	
-	// Tao cai nut confirm roi add vao truoc nut btnCancel
-
-	var confirmBtn = document.createElement('input');
-	confirmBtn.className = 'btn btn-success btn-block';
-
-	// confirmBtn.setAttribute('onclick', 'onConfirmEdit(this)');
-	confirmBtn.addEventListener('click', onConfirmEdit);
-	confirmBtn.setAttribute('type', 'button');
-	confirmBtn.value = 'Confirm';
-	
-	btnAdd.parentNode.insertBefore(confirmBtn, document.getElementById('btnCancel') );
-	;
+function searchInput(){
+let value = document.getElementById('search').value;
 
 }
-
-var currentEditEl;
-
-function onConfirmEdit(event) {
-	var confirmBtn = event.target;
-	// confirmBtn.previousElementSibling.setAttribute('style', 'display: block');
-	confirmBtn.remove();
-	document.getElementById('btnAdd').setAttribute('style', 'display: block');
-	console.log(currentEditEl);
-	
-	// lay gia tri thay doi cua form input -> ghi vao currentEditEl
-	currentEditEl.cells[0].innerHTML = document.getElementById('fullname').value;
-	currentEditEl.cells[1].innerHTML = document.getElementById('phone-number').value;
-	currentEditEl.cells[2].innerHTML = document.getElementById('address').value;
-
-	// location.reload();
-
+function render(list){
+list.map(item =>{
+	listArrayName.push(`<td>${item.name}</td>
+	<td>${item.phone}</td>
+	<td>${item.address}</td>
+	<td>
+		<button class="btn btn-danger" onclick="deleteRow(event)" style="display:none">x</button>
+		<button class="btn btn-warning editRowBtn" onclick="onEdit(this)">Edit</button>
+		<button class="btn btn-danger deleteRowBtn">x</button>
+	</td>`);
+})
 }
-
-function deleteRow(e) {
-	console.log(e);
-	var eventEl = e.target;
-	var parent2 = eventEl.parentNode.parentNode;
-	parent2.remove();
-}
+// console.log(document.getElementById("search"));
+// document.getElementById("search").addEventListener('onchange',()=>{
+// 	let value = document.getElementById('search').value;
+// 	listName.filter(item)
+// });
