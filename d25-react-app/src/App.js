4bf547049c068dcb5e0270a0f3cac87c49b8ddd0
Hieu <hieu2906090@gmail.com>
2021-01-12 11:08:07 +0700

    learn react basic

---
+++ d25-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./page/HomePage";
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  ); // JSX
+}
+
+export default App;
